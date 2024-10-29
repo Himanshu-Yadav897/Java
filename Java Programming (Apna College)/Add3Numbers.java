e0db4ae56974f3bf5320092db1f731c653c3db4e
Himanshu-Yadav897 <116517982+Himanshu-Yadav897@users.noreply.github.com>
2024-10-29 19:51:43 +0530

    Add files via upload

---
+++ Java Programming (Apna College)/Add3Numbers.java
@@ -0,0 +1,10 @@
+// Program to add 3 Numbers
+public class Add3Numbers {
+    public static void main(String args[]){
+        int num1 = 3;
+        int num2 = 7;
+        int num3 = 3;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
